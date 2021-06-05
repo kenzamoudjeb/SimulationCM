4fd6037609023ab8d84dfa38f7e4c0afc04e88aa
belhoul-imene <84587777+belhoul-imene@users.noreply.github.com>
2021-06-05 11:11:30 +0100

    Add files via upload

--- R/rdist.R
+++ R/rdist.R
@@ -1,9 +1,13 @@
+#' simulation d une loi discrete
+#' @export
+#' @param x numeric vector representing the values of the random variable
+#' @param p numeric vector representing the probabilities
 rdist<-function(x,p)
 {
   n=length(p)
   r=runif(1)
   b=p[1];
-  if((r>=0) & (r<=b))
+  if((r>0)&(r<=b))
   {
     y=x[1]
     return(y)
@@ -12,9 +16,9 @@ rdist<-function(x,p)
   {
     a=p[1]
     b=b+p[2]
-    for(i in 2:n)
+    for(i in 2:n-1)
     {
-      if((r>=a) & (r <=b))
+      if((r>=a)&(r<=b))
       {
         y=x[i]
         return(y)
